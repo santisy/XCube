351ba2264d7535d926d45f426fb94b12b581b41c
Anonymous Authors <N/A>
2024-11-18 16:48:59 -0800

    new script setup

---
+++ infer_recon.py
@@ -0,0 +1,92 @@
+import importlib
+import argparse
+import glob
+import os
+import yaml
+
+import torch
+from easydict import EasyDict as edict
+from pathlib import Path
+from tqdm import tqdm
+
+import nksr
+import trimesh
+from xcube.models.autoencoder import Model as VAE
+from xcube.data.base import DatasetSpec as DS
+from xcube.utils import exp
+
+def create_model_from_args(config_path, ckpt_path, strict=True):
+    model_yaml_path = Path(config_path)
+    model_args = exp.parse_config_yaml(model_yaml_path)
+    net_module = importlib.import_module("xcube.models." + model_args.model).Model
+    args_ckpt = Path(ckpt_path)
+    assert args_ckpt.exists(), "Selected checkpoint does not exist!"
+    net_model = net_module.load_from_checkpoint(args_ckpt, hparams=model_args, strict=strict)
+    return net_model.eval()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ckpt", required=True, type=str)
+parser.add_argument("--input_dir", required=True, type=str)
+parser.add_argument("--output_dir", required=True, type=str)
+parser.add_argument("--class_label", required=True, type=str)
+
+args = parser.parse_args()
+
+# Prepare directories
+input_dir = args.input_dir
+output_dir = args.output_dir
+os.makedirs(output_dir, exist_ok=True)
+
+if args.class_label == "chair":
+    config_nksr = "configs/shapenet/chair/train_nksr_refine.yaml"
+    ckpt_nksr = "checkpoints/chair/nksr_refine/last.ckpt"
+elif args.class_label == "plane":
+    config_nksr = "configs/shapenet/plane/train_nksr_refine.yaml"
+    ckpt_nksr = "checkpoints/plane/nksr_refine/last.ckpt"
+elif args.class_label == "objaverse":
+    config_nksr = "configs/objaverse/train_nksr_refine.yaml"
+    ckpt_nksr = "checkpoints/objaverse/nksr_refine/last.ckpt"
+
+# Construct the reconstructor
+reconstructor = create_model_from_args(config_nksr, ckpt_nksr, strict=False).cuda()
+
+input_files = glob.glob(os.path.join(input_dir, "*.pkl"))
+
+with open('configs/train/vae/vae_128x128x128_sparse.yaml', 'r') as file:
+    hparams = edict(yaml.safe_load(file))
+if args.class_label == "objaverse":
+    with open('configs/objaverse/train_vae_128x128x128_sparse.yaml', 'r') as file:
+        hparams_v2 = edict(yaml.safe_load(file))
+    hparams.update(hparams_v2)
+
+hparams.pretrained_weight = args.ckpt
+vae = VAE(hparams).cuda()
+
+# Data preparation
+for data_path in tqdm(input_files):
+    data_name = os.path.basename(data_path).split(".")[0]
+    input_data = torch.load(data_path)
+
+    data_dict = {}
+    data_dict[DS.INPUT_PC] = [input_data['points'],]
+    data_dict[DS.TARGET_NORMAL] = [input_data['normals'],]
+
+    out_dict = edict()
+    vae(data_dict, out_dict)
+
+    output_x = out_dict['x']
+    res = out_dict
+
+    # Export the mesh
+    batch_idx = 0
+    pd_grid = output_x.grid[batch_idx]
+    pd_normal = res.normal_features[-1].feature[batch_idx].jdata
+    with torch.no_grad():
+        field = reconstructor.forward({'in_grid': pd_grid, 'in_normal': pd_normal})
+    field = field['kernel_sdf']
+    mesh = field.extract_dual_mesh(max_depth=0, grid_upsample=2)
+    # save mesh
+    mesh_save = trimesh.Trimesh(vertices=mesh.v.cpu().numpy(), faces=mesh.f.cpu().numpy())
+    # mesh_save.merge_vertices()
+    mesh_save.export(os.path.join(output_dir, f"{data_name}_XCubeRecon.obj"))
+
---
+++ prepare_data.py
@@ -0,0 +1,111 @@
+
+import glob
+import os
+import point_cloud_utils as pcu
+import numpy as np
+from tqdm import tqdm
+import fvdb
+import torch
+import argparse
+
+args = argparse.ArgumentParser()
+args.add_argument('--data_root', type=str, default='../data/shapenet_manifold')
+args.add_argument('--target_root', type=str, default='../data/shapenet/')
+args.add_argument('--num_vox', type=int, default=512)
+args = args.parse_args()
+
+data_root = args.data_root
+target_root = args.target_root
+
+num_vox = args.num_vox
+
+if num_vox > 512:
+    max_num_vox = num_vox
+    sample_pcs_num = 5_000_000
+else:
+    max_num_vox = 512
+    sample_pcs_num = 1_000_000
+vox_size = 1.0 / max_num_vox
+
+target_dir = target_root
+os.makedirs(target_dir, exist_ok=True)
+
+model_list = glob.glob(os.path.join(data_root, "*.obj"))
+
+for model_path in tqdm(model_list):
+    model_name = os.path.basename(model_list).split(".")[0]
+    target_path = os.path.join(target_dir, f"{model_name}.pkl")
+    # check if target_path exist
+    if os.path.exists(target_path):
+        continue
+    
+    v, f = pcu.load_mesh_vf(os.path.join(model_path))
+    
+    try:
+        fid, bc = pcu.sample_mesh_random(v, f, sample_pcs_num)
+        ref_xyz = pcu.interpolate_barycentric_coords(f, fid, bc, v)
+    except:
+        fid, bc = pcu.sample_mesh_random(v, f, sample_pcs_num)
+        ref_xyz = pcu.interpolate_barycentric_coords(f, fid, bc, v)
+
+    n = pcu.estimate_mesh_face_normals(v, f)
+    ref_normal = n[fid]
+            
+    ijk = pcu.voxelize_triangle_mesh(v, f.astype(np.int32), vox_size, np.zeros(3))
+    grid = fvdb.sparse_grid_from_ijk(fvdb.JaggedTensor([torch.from_numpy(ijk).cuda()]), voxel_sizes=vox_size, origins=[vox_size / 2.] * 3)
+    
+    # get normal ref
+    ref_xyz = torch.from_numpy(ref_xyz).float().cuda()
+    ref_normal = torch.from_numpy(ref_normal).float().cuda()
+    input_normal = grid.splat_trilinear(fvdb.JaggedTensor(ref_xyz), fvdb.JaggedTensor(ref_normal))
+    # normalize normal
+    input_normal.jdata /= (input_normal.jdata.norm(dim=1, keepdim=True) + 1e-6) # avoid nan
+
+    # _, f_idx, _ = pcu.closest_points_on_mesh(
+    #     grid.grid_to_world(grid.ijk.float()).jdata.cpu().numpy().astype(float), v.astype(float), f)
+    # input_normal = fvdb.JaggedTensor([torch.from_numpy(n[f_idx])])        
+            
+    # normalize xyz to conv-onet scale
+    xyz = grid.grid_to_world(grid.ijk.float()).jdata
+    xyz_norm = xyz * 128 / 100
+    ref_xyz = ref_xyz * 128 / 100
+    
+    # convert to fvdb_grid format
+    if num_vox == 512:
+        # not splatting
+        target_voxel_size = 0.0025
+        target_grid = fvdb.sparse_grid_from_points(
+                fvdb.JaggedTensor(xyz_norm), voxel_sizes=target_voxel_size, origins=[target_voxel_size / 2.] * 3)
+    elif num_vox == 16:
+        # splatting
+        target_voxel_size = 0.08
+        target_grid = fvdb.sparse_grid_from_nearest_voxels_to_points(
+                    fvdb.JaggedTensor(xyz_norm), voxel_sizes=target_voxel_size, origins=[target_voxel_size / 2.] * 3)
+    elif num_vox == 128:
+        # splatting
+        target_voxel_size = 0.01
+        target_grid = fvdb.sparse_grid_from_nearest_voxels_to_points(
+                    fvdb.JaggedTensor(xyz_norm), voxel_sizes=target_voxel_size, origins=[target_voxel_size / 2.] * 3)
+    elif num_vox == 256:
+        target_voxel_size = 0.005
+        target_grid = fvdb.sparse_grid_from_nearest_voxels_to_points(
+                    fvdb.JaggedTensor(xyz_norm), voxel_sizes=target_voxel_size, origins=[target_voxel_size / 2.] * 3)
+    elif num_vox == 1024:
+        target_voxel_size = 0.00125
+        target_grid = fvdb.sparse_grid_from_points(
+                    fvdb.JaggedTensor(xyz_norm), voxel_sizes=target_voxel_size, origins=[target_voxel_size / 2.] * 3)
+    else:
+        raise NotImplementedError
+    
+    # get target normal
+    target_normal = target_grid.splat_trilinear(fvdb.JaggedTensor(ref_xyz), fvdb.JaggedTensor(ref_normal))
+    target_normal.jdata /= (target_normal.jdata.norm(dim=1, keepdim=True) + 1e-6)
+
+    save_dict = {
+        "points": target_grid.to("cpu"),
+        "normals": target_normal.cpu(),
+        "ref_xyz": ref_xyz.cpu(),
+        "ref_normal": ref_normal.cpu(),
+    }
+    
+    torch.save(save_dict, target_path)
